4b812c2fd2539dba7eeabd08845f0ef678dc36cc
AndreaSalvatelli <levelup.cef@gmail.com>
2026-04-26 22:11:05 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import IAOperativaLanding from "./IAOperativaLanding.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <IAOperativaLanding />
+  </React.StrictMode>
+);
