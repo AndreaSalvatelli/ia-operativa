ba26804981e28be942abcfc6b886a7e7f7624687
AndreaSalvatelli <levelup.cef@gmail.com>
2026-05-10 22:46:08 +0200

    Add header to blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { articles } from "../data/articles";
 import BlogCard from "../components/BlogCard";
+import SiteHeader from "../components/SiteHeader";
 
 export default function Blog() {
   return (
-    <main className="blogPage">
-      <div className="container">
-        <header className="blogHeader">
-          <p className="blogEyebrow">Blog IA Operativa</p>
-          <h1>Approfondimenti pratici su AI e processi aziendali</h1>
-          <p>
-            Analisi sobrie e operative per imprenditori, professionisti e team PMI che vogliono usare l'intelligenza artificiale in modo concreto.
-          </p>
-        </header>
+    <>
+      <SiteHeader />
 
-        <section className="blogGrid" aria-label="Elenco articoli">
-          {articles.map((article) => (
-            <BlogCard key={article.slug} article={article} />
-          ))}
-        </section>
-      </div>
-    </main>
+      <main className="blogPage">
+        <div className="container">
+          <header className="blogHeader">
+            <p className="blogEyebrow">Blog IA Operativa</p>
+            <h1>Approfondimenti pratici su AI e processi aziendali</h1>
+            <p>
+              Analisi sobrie e operative per imprenditori, professionisti e team PMI che vogliono usare l'intelligenza artificiale in modo concreto.
+            </p>
+          </header>
+
+          <section className="blogGrid" aria-label="Elenco articoli">
+            {articles.map((article) => (
+              <BlogCard key={article.slug} article={article} />
+            ))}
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
